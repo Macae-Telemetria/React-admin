d4a3b4a0b9d33efc54a6315b62c3f696447639ca
fonsecaBarreto <lucasbfonte@gmail.com>
2024-05-04 00:14:29 -0300

    refactor: dashboard

--- ui/apps/admin/pages/Dashboard/index.tsx
+++ ui/apps/admin/pages/Dashboard/index.tsx
@@ -3,7 +3,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 import { PAGES_MANIFEST } from "..";
-import { Card } from "antd";
+import { Card, Flex, Table, Tag, Typography } from "antd";
+const { Text } = Typography;
+
+import {
+  UserOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  CloudUploadOutlined,
+} from "@ant-design/icons";
+
 export const DashboardPage = () => {
   return (
     <PageScaffold
@@ -13,17 +22,56 @@ export const DashboardPage = () => {
       <div className="dashboard-grid">
         <section>
           <Card style={{ width: `100%` }}>
-            <Link to={PAGES_MANIFEST.SoftwareReleases.path}>
-              Software releases
-            </Link>
+            <Flex gap={12}>
+              <UploadOutlined style={{ fontSize: "24px" }} />
+              <Link to={PAGES_MANIFEST.SoftwareReleases.path}>
+                <Typography.Title level={5} style={{ margin: 0 }}>
+                  Software releases
+                </Typography.Title>
+              </Link>
+            </Flex>
           </Card>
         </section>
 
         <section>
           <Card style={{ width: `100%` }}>
-            <Link to={PAGES_MANIFEST.Ota.path}> OTA Update </Link>
+            <Flex gap={12}>
+              <CloudUploadOutlined style={{ fontSize: "24px" }} />
+              <Link to={PAGES_MANIFEST.Ota.path}>
+                <Typography.Title level={5} style={{ margin: 0 }}>
+                  OTA Update
+                </Typography.Title>
+              </Link>
+            </Flex>
           </Card>
         </section>
+
+       {/*  <section>
+          <Card style={{ width: `100%` }}>
+            <Flex gap={12}>
+              <TeamOutlined style={{ fontSize: "24px" }} />
+
+              <Link to={PAGES_MANIFEST.Ota.path}>
+                <Typography.Title level={5} style={{ margin: 0 }}>
+                  Organizações
+                </Typography.Title>
+              </Link>
+            </Flex>
+          </Card>
+        </section>
+
+        <section>
+          <Card style={{ width: `100%` }}>
+            <Flex gap={12}>
+              <UserOutlined style={{ fontSize: "24px" }} />
+              <Link to={PAGES_MANIFEST.Ota.path}>
+                <Typography.Title level={5} style={{ margin: 0 }}>
+                  Usuarios
+                </Typography.Title>
+              </Link>
+            </Flex>
+          </Card>
+        </section> */}
       </div>
     </PageScaffold>
   );
